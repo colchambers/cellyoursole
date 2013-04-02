9e753ecb97318069ab1210d5e71d4110accf424a
Colin Chambers <colchambers@gmail.com>
2013-04-02 21:00:17 +0100

    Changed pause button label. Test of lose screen

--- Assets/Pages/Tennis/Serve/TennisServeOptions.js
+++ Assets/Pages/Tennis/Serve/TennisServeOptions.js
@@ -63,7 +63,7 @@ class TennisServeOptions extends Options {
 	function initialiseToggle(){
 		// Add mode button
 		var r: iGUIRoot = mainPresenter.panel.getContainer("root");
-		var button: iGUIButton = addPageButton(this.id, 'O', 'instructionsButton', r);
+		var button: iGUIButton = addPageButton(this.id, 'P', 'instructionsButton', r);
 		button.setX(0.13);
 		button.setY(0);
 		button.setWidth(0.11);
@@ -140,6 +140,8 @@ class TennisServeOptions extends Options {
 	function initialiseScore(){
 		score = 0;
 		recordHit();
+		mainPresenter.mvpShow(this.id);
+		quit();
 	}
 	
 	function initialiseTimers(){
@@ -196,8 +198,7 @@ class TennisServeOptions extends Options {
 		button.clickCallback = positionZones_Click;
 		
 		addPageNavigationButton('views', 'Views');
-		addPageNavigationButton('elements', 'Elements');
-		addPageNavigationButton('scenarios', 'Scenarios');
+		addPageNavigationButton('quit', 'Quit');
 		
 		// Disable background scene.
 		setBackgroundEnabled(false);
@@ -211,15 +212,8 @@ class TennisServeOptions extends Options {
 			case 'views':
 				populateViews();
 				break;
-			case 'elements':
-				populateElements();
-				break;
-			case 'scenarios':
-				populateScenarios();
-				break;
-			case 'updateScenario':
-				var item = scenarioModal.getCurrentItem();
-				updateScenario(item.id);
+			case 'quit':
+				quit();
 				break;
 		}
 		super(id);
@@ -252,162 +246,36 @@ class TennisServeOptions extends Options {
 		addPageViewButton('Walking Camera 2', 'First Person 2');
 		addPageViewButton('Top Camera', 'Top');
 	}
-	
+
 	/**
-	 * Populate the element menu 
+	 * Populate the lose menu 
 	 * @return void
 	 */
-	function populateElements(){
+	function populateLose(){
+		setPaused(true);
 		prepareSubPage();
-		setBackgroundEnabled(true);
+		Debug.Log('lose');
 		
 		var text="";
 		
-		text ="Using this resource you can organise a tennis court and explore how your ";
-		text+="changes affect its accessibility. \n";
-		text+="You can move most elements by dragging them.\n";
-		text+="To drag an item select it with your finger or mouse and drag it.\n";
+		text ="You lose.";
+		text+="Select different view points from the list below.\n";
 			
 		addPageText(text, 0.5);
-		
-		var elements: Dictionary.<String, String> = new Dictionary.<String, String>();
-		
-		// All
-		elements.Add(ELEMENTS_SWITCH_ALL_ID, 'All');
-		
-		// Dynamic Objects
-		elements.Add('Player 1', 'Player 1');
-		elements.Add('Player 2', 'Player 2');
-		elements.Add('Player 3', 'Player 3');
-		elements.Add('Player 4', 'Player 4');
-		elements.Add('ball', 'Ball');
-
-		elementSwitches = new Dictionary.<String, iGUISwitch>();
-		for (var o in elements) {
-			elementSwitches.Add( o.Key,
-			addPageSwitch(o.Key, o.Value, handleElementSwitch_change));
-		}
-
-		updateElementSwitches();	
 	}
-
+	
 	/**
-	 * Populate the settings menu 
+	 * Quit lesson.
 	 * @return void
 	 */
-	function populateScenarios(){
-		setBackgroundEnabled(true);
-		prepareSubPage();
+	function quit(){
+		Debug.Log('quit');
+		// Quit lesson.
 		
-		var text="";
-		var item: TennisScenarioItem;
-		
-		text ="Load any scenario below into this scene";
-		addPageText(text, 0.5);
-		
-		var button : iGUIButton;
-		button = addPageButton('0', 'New Scenario');
-		button.clickCallback = updateScenario_Click;
-		
-		//button = addPageButton('0', 'New Screenshot');
-		//button.clickCallback = createScreenshot_Click;
-		
-		var row: iGUIListBox;
-		var color: Color;
-		var currentItem = scenarioModal.getCurrentItem();
-		// Loop through and display scenarios
-		for(item in scenarioModal.items.Values){
-			color = currentItem.id == item.id?Color.red:Color.white;
-	    	row = addPageListBox(list);
-	    	row.setWidth(1);
-	    	row.setHeight(0.17);
-	    	row.direction = iGUIListDirection.LeftToRight;
-	    	
-	    	button = addPageButton(item.id.ToString(), item.getField('name'), 'instructionsButton', row);
-	    	button.clickCallback = loadScenario_Click;
-	    	button.setWidth(0.7);
-	    	button.labelColor = color;
-	    	
-	    	button = addPageButton(item.id.ToString(), 'U', 'instructionsButton', row);
-	    	button.setWidth(0.1);
-	    	button.clickCallback = updateScenario_Click;
-	    	button.labelColor = color;
-	    	
-	    	button = addPageButton(item.id.ToString(), 'X', 'instructionsButton', row);
-	    	button.setWidth(0.1);
-	    	button.labelColor = color;
-	    	button.clickCallback = deleteScenario_Click;
-		}
+		// Return to menu.
+		populateLose();
 	}
-	
-	function updateScenario(id: int){
-		setBackgroundEnabled(false);
-		prepareSubPage('scenarios');
-		
-		var item = getScenario(id);
-		var textField: iGUITextfield;
-		textField = addPageTextField('Name', item.getField('name'), 0.12, list);
-		panel.addField('name', textField);
-		var button : iGUIButton;
-		button = addPageButton(item.id.ToString(), 'Save');
-    	button.clickCallback = saveScenario_Click;
-    	button.setWidth(0.7);
-	}
-	
-	function saveScenario(id: int){
-		var item = scenarioModal.getItemById(id);
-		scenarioModal.saveItem(item);
-	}
-	
-	function deleteScenario(id: int){
-		scenarioModal.deleteItem(id);
-		loadPage('scenarios');
-	}
-	
-	function createScreenshot(id: int){
-		var rootPanel: iGUIElement = panel.rootPanel;
-		var panelId: int = mainPresenter.getPanelIdFromPanel(rootPanel);
-		mainPresenter.disablePanel(panelId);
-		scenarioModal.createScreenshot(id);
-		
-		/*
-		 * Screenshot is rendered at end of frame. Can't hide, then 
-		 * show GUI. Add a 'finished' button instead to show GUI.
-		 */
-		// Add 'finished' button
-		var r: iGUIRoot = mainPresenter.panel.getContainer("root");
-		var button: iGUIButton = addPageButton(panelId.ToString(), 'X', 'instructionsButton', r);
-		button.setX(0.39);
-		button.setY(0);
-		button.setWidth(0.11);
-		button.clickCallback = finishedScreenshot_Click;
-		
-		panel.addButton('finishedScreenshot', button);
-	}
-	
-	function finishedScreenshot_Click(caller : iGUIButton){
-		mainPresenter.enablePanel(int.Parse(caller.userData));
-		
-		// remove 'finished button;
-		var buttonId = 'finishedScreenshot';
-		var button: iGUIButton = panel.getButton(buttonId);
-		var r: iGUIRoot = mainPresenter.panel.getContainer("root");
-		r.removeElement(button);
-		panel.removeButton(buttonId);
-	}
-		
-	function updateElementSwitches(){
-		// All switch
-		var s: iGUISwitch = elementSwitches[ELEMENTS_SWITCH_ALL_ID];
-			s.setValue(elementAllEnabled);
-		var items = getSceneItems();	
-		for (var item in items.Values) {
-			var o = item.item;
-			s = elementSwitches[o.name];
-			s.setValue(o.active);
-		}	
-	}
-	
+			
 	function addPageViewButton (id: String, t: String) {
 		var b: iGUIButton = addPageButton (id, t, 'instructionsButton');
 		b.clickCallback = setView_click;
@@ -417,16 +285,6 @@ class TennisServeOptions extends Options {
 	function backToScene_Click(caller : iGUIButton){
 		setPaused(false);
 		mainPresenter.mvpToggle(this.id);
-		//mainPresenter.disablePanel(11);
-		
-	}
-	
-	function positionZones_Click(caller : iGUIButton){
-		var positionZone = GameObject.Find('Position Zone');
-		Debug.Log(positionZone);
-		positionZone.GetComponent(MeshCollider).enabled = true;
-		positionZone.GetComponent(MeshRenderer).enabled = true;
-		
 	}
 	
 	function setView_click (caller: iGUIButton) {
@@ -436,213 +294,6 @@ class TennisServeOptions extends Options {
 		si.currentCamera = c;
 		si.dist = c.transform.localPosition.z;
 		si.currentCamera.enabled = true;
-	}
-	
-	function handleElementSwitch_change (caller: iGUISwitch) {
-		if(caller.userData == 'all'){
-			elementAllEnabled = !elementAllEnabled;
-			var items = getSceneItems();	
-			for (var item in items.Values) {
-				var o = item.item;
-				toggleObjectActive(o.name);
-			}
-			updateElementSwitches();
-			return;
-		}
-		
-		toggleObjectActive(caller.userData);
-	}
-	
-	function toggleObjectActive(id){
-		var items = getSceneItems();	
-		for (var item in items.Values) {
-			var o = item.item;
-			if(o.name != id){
-				continue;
-			}
-			o.SetActiveRecursively(!o.active);
-			return;
-		}
-	}
-	
-	/*
-	 * Scenarios
-	 */
-	function addPageScenarioLoadButton (id: String, t: String) {
-		var b: iGUIButton = addPageButton (id, t, 'instructionsButton');
-		b.clickCallback = loadScenario_Click;
-		return b;
-	}
-	
-	function loadScenario_Click(caller : iGUIButton){
-		loadScenario(int.Parse(caller.userData));
-		loadPage('scenarios');
-	}
-	
-	function loadScenario(id:int){
-		var item = getScenario(id);
-		scenarioModal.setCurrentItem(item);
-		JSONLevelSerializer.LoadNow(item.getField('data'), true, true, loadScenario_Complete);
-	}
-	
-	function loadScenario_Complete(){
-		Debug.Log('Load scenario complete.');
-		refreshScenario();
-	}
-	
-	function refreshScenario(){
-	
-		var items = getSceneItems();
-		var sc: SphereCollider;
-		var cc: CapsuleCollider;
-		for(var item in items.Values){
-		
-			// Adjust the ball colllider
-			if(item.id == 'ball'){
-				sc = item.item.collider;
-				sc.radius = 8.0;
-			}
-			
-			if(item.item.tag == 'Player'){
-				cc = item.item.collider;
-				cc.radius = 1.0;
-				cc.height = 3.0;
-			}
-			
-			
-		}
-	}
-	
-	function updateScenario_Click(caller : iGUIButton){
-		updateScenario(int.Parse(caller.userData));
-	}
-	
-	function createScreenshot_Click(caller : iGUIButton){
-		createScreenshot(int.Parse(caller.userData));
-	}
-	
-	function deleteScenario_Click(caller : iGUIButton){
-		deleteScenario(int.Parse(caller.userData));
-	}
-		
-	function saveScenario_Click(caller : iGUIButton){
-		var id: int = int.Parse(caller.userData);
-		validateScenario(id);
-		if(e.hasErrors()){
-			displayErrors();
-			return;
-		}
-		e.clearErrors();
-		var item:TennisScenarioItem = getScenario(id);
-		var f: String = 'name';
-		var name: iGUITextfield = panel.getField(f);
-		item.setField(f, name.value);
-		
-		// If it's the loaded scenario save layout data
-		Debug.Log('id = '+id);
-		
-		var currentItem = scenarioModal.getCurrentItem();
-		Debug.Log('currentItem = '+currentItem);
-		if(!item.getField('data') || id == currentItem.id){
-			item.setField('data', JSONLevelSerializer.SerializeLevel());
-		}
-		saveScenario(id);
-		
-		loadPage('scenarios');
-	}
-		
-	function getScenario(id:int){
-		return scenarioModal.getItemById(id);
-	}
-	
-	function populateField(f: iGUIElement, t: TennisScenarioItem, fn: String, ft: String){
-		f.userData = new UserData(t.id, fn, "tennisScenario", ft);
-		
-		var fv = t.getField(fn);
-		v.setElementValue(f, fv.ToString());
-		var u: UserData = f.userData;
-		switch(u.fieldType){
-			case "text":
-				var tf: iGUITextfield = f;
-				tf.blurCallback = form_field_blur;
-				tf.focusCallback = form_field_focus;
-				break;
-			case "textarea":
-				var ta: iGUITextarea = f;
-				ta.blurCallback = form_field_blur;
-				ta.focusCallback = form_field_focus;
-				break;
-			case "number":
-				var nf: iGUINumberField = f;
-				nf.blurCallback = form_field_blur;
-				nf.focusCallback = form_field_focus;
-				break;
-		}
-		
-	}
-	
-	function form_field_blur(caller: iGUIElement){
-		var u: UserData = caller.userData;
-		var fv = getView().getElementValue(caller);
-		u.value = fv;
-		scenarioUpdateHandler(u);
-	}
-	
-	function form_field_value_change_dropdownlist(caller: iGUIElement){
-		var field: iGUIDropDownList = caller;
-		var userData: UserData = field.userData;
-		userData.value = field.options[field.selectedIndex].text;
-		scenarioUpdateHandler(userData);
-	}
-	
-	function scenarioUpdateHandler(ud: UserData){
-		var item = scenarioModal.items[ud.id];
-		item.setField(ud.field, ud.value);
-	}
-	
-	function form_field_focus(caller : iGUIElement){
-		var mainView = getView();
-		var fv = mainView.getElementValue(caller);
-		mainView.openTouchKeyboard(fv.ToString());
-	}
-	
-	function form_Done_Click(caller : iGUIButton){
-		validateScenario(caller.userData);
-		if(e.hasErrors()){
-			displayErrors();
-			return;
-		}
-		e.clearErrors();
-	
-		saveScenarioAndDisplayList();
-	}
-	
-	function saveScenarioAndDisplayList(){
-		scenarioModal.save();
-		//tasksList_Init(tasksList);
-		//v.enablePanel(v.panelIds[3]);
-	}
-	
-	
-	function validateScenario(id){
-		var ok = true;
-		var scenario: TennisScenarioItem = scenarioModal.getItemById(id);
-		var nameField: iGUITextfield = panel.getField("name");
-		Debug.Log('nameField.value = '+nameField.value);
-		/*
-		e.window.label.text = "Notifications";
-		if(!nameField.value){
-			ok = false;
-			e.addError(nameField.variableName, "Name field cannot be empty");
-		}
-		
-		var nameMaxLength: int = 24;
-		if(nameField.value && nameField.value.Length>nameMaxLength){
-			ok = false;
-			e.addError(nameField.variableName, "Name field cannot be longer than "+nameMaxLength+" characters.");
-		}*/
-		
-		return ok;
 	}
 	
 	var instruction: boolean=false;
